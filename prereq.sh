63e71c0c2472c2a1b72fb712ba8a050b66a64c4b
ghastman <67092919+ghastman@users.noreply.github.com>
2025-02-18 11:55:51 -0600

    Update prereq.sh

--- prereq.sh
+++ prereq.sh
@@ -1,3 +1,4 @@
+sudo dnf install git
 sudo dnf install libXext-devel libXft-devel libXinerama-devel libXmu-devel libXpm-devel libXrandr-devel libXrender-devel libjpeg-devel 
 sudo dnf install ImageMagick-devel automake chrpath fontconfig-devel gettext-devel giflib-devel libICE-devel libSM-devel libX11-devel 
 sudo dnf install libpng-devel libtiff-devel libtool pango-devel xorg-x11-proto-devel
