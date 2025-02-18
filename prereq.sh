e2785e650624174ae3875125c0095dcb1abbd432
ghastman <67092919+ghastman@users.noreply.github.com>
2025-02-18 13:49:25 -0600

    Update prereq.sh
    List of all the packages I use to get up and running

--- prereq.sh
+++ prereq.sh
@@ -1,4 +1,5 @@
 sudo dnf install git
+sudo dnf install rpmdevtools rpmlint  
 sudo dnf install libXext-devel libXft-devel libXinerama-devel libXmu-devel libXpm-devel libXrandr-devel libXrender-devel libjpeg-devel 
 sudo dnf install ImageMagick-devel automake chrpath fontconfig-devel gettext-devel giflib-devel libICE-devel libSM-devel libX11-devel 
 sudo dnf install libpng-devel libtiff-devel libtool pango-devel xorg-x11-proto-devel
