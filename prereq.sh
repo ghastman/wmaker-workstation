03aa53a6446cda3380dbc1395c76a420cd3dd10d
ghastman <67092919+ghastman@users.noreply.github.com>
2025-02-18 14:23:30 -0600

    Update prereq.sh

--- prereq.sh
+++ prereq.sh
@@ -5,4 +5,4 @@ git \
 rpmdevtools rpmlint make \
 libXext-devel libXft-devel libXinerama-devel libXmu-devel libXpm-devel libXrandr-devel libXrender-devel libjpeg-devel \
 ImageMagick-devel automake chrpath fontconfig-devel gettext-devel giflib-devel libICE-devel libSM-devel libX11-devel \
-libpng-devel libtiff-devel libtool pango-devel xorg-x11-proto-devel
+libpng-devel libtiff-devel libtool pango-devel xorg-x11-proto-devel xterm
