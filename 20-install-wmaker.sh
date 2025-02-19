f1c0537faebb246bee46c5e8c6cb35564b7f2fcf
Lance Blackwood <ghastman@gmail.com>
2025-02-18 23:54:05 -0600

    update paths

--- 20-install-wmaker.sh
+++ 20-install-wmaker.sh
@@ -1,3 +1,3 @@
 # Window Maker from the RPM build
-sudo dnf install ~/Workspace/RPMbuild/RPMS/x86_64/WINGs-libs-0.96.0-3.el8.x86_64.rpm
-sudo dnf install ~/Workspace/RPMbuild/RPMS/x86_64/WindowMaker-0.96.0-3.el8.x86_64.rpm
+sudo dnf install ~/Workspace/wmaker-workstation/rpmbuild/RPMS/x86_64/WINGs-libs-0.96.0-3.el8.x86_64.rpm
+sudo dnf install ~/Workspace/wmaker-workstation/rpmbuild/RPMS/x86_64/WindowMaker-0.96.0-3.el8.x86_64.rpm
